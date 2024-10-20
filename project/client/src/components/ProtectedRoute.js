15a67ddb00088d517533199d7be8f01e2cc885e5
sanjith <vgsanjith166@gmail.com>
2024-10-20 19:33:09 +0530

    nav bar created

--- project/client/src/components/ProtectedRoute.js
+++ project/client/src/components/ProtectedRoute.js
@@ -1,19 +1,44 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 import { showLoading, hideLoading } from '../redux/loadersSilce'
 import { getCurrentUser } from '../apicalls/users';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { setUser} from '../redux/userSilce'
+import { setUser} from '../redux/userSilce';
+import { Layout, Menu } from 'antd';
+import { Header } from 'antd/es/layout/layout';
+import { HomeOutlined, UserAddOutlined, UserOutlined, LogoutOutlined} from '@ant-design/icons';
 
 function ProtectedRoute({children}){
 
-    const { user } = useSelector((state)=> state.user)
+    const { user } = useSelector((state)=> state.user);
 
-    console.log(user)  // => this will display the detail of current use who logged in now.
+    // console.log(user)  // => this will display the detail of current use who logged in now.
 
     // Validate the user 
     const dispatch = useDispatch();
     const navigate = useNavigate();
+
+    const navItems = [
+        {
+            label : 'Home',
+            icon : <HomeOutlined />
+        },
+        {
+            label : `${user ? user.name.charAt(0).toUpperCase() + user.name.slice(1) : ""}`,           // charAt(0) => takes first letter of user name
+            icon : <UserAddOutlined />,                                                                // slice(1)  => add remaing letter as it is         
+
+            children : [
+                {
+                    label : 'Profile',
+                    icon : <UserOutlined />
+                },
+                {
+                    label : (<Link to="/login" onClick={()=>localStorage.removeItem('token')}> Log out </Link>),
+                    icon : <LogoutOutlined />
+                }
+            ]
+        }
+    ]
 
     const getValidUser = async() =>{
         try{
@@ -33,15 +58,35 @@ function ProtectedRoute({children}){
         }else{
             navigate('/login')
         }
-    },[])
+    }, [])
 
     return(
-    <>
-    <div> {children} </div>  
-    <h1> {user} </h1>   
-    </>
-        
+    user && (
+        <>
+
+        <Layout>
+            <Header className='d-flex justify-content-between'
+            style={{
+                position : "sticky",
+                top : 0,
+                zIndex : 1,
+                width : "100%",
+                display : "flex",
+                alignItems : "center",
+            }}>
+                <h2 className='demo-logo text-white m-0' style={{ color: "white"}}>Booky My Show</h2>
+                <Menu theme="dark" mode='horizontal' items={navItems}></Menu>
+            </Header>
+
+            <div style={{ padding:24, minHeight: 380, background: "gray"}}>
+                {children}
+            </div>    
+
+        </Layout>
+
+        </>
     )
+)
 }
 
 export default ProtectedRoute;
--- project/server/middlewares/authMiddleware.js
+++ project/server/middlewares/authMiddleware.js
@@ -5,9 +5,9 @@ module.exports = function(req, res, next){
     try{
 
         const token = req.headers.authorization.split(" ")[1];
-        console.log(token)
-        const verifiedToken = jwt.verify(token, process.env.SECRET_KEY)
-        console.log(verifiedToken)
+        // console.log(token)
+        const verifiedToken = jwt.verify(token,`${process.env.SECRET_KEY}` )
+        // console.log(verifiedToken)
         req.body.userId = verifiedToken.userId;
     
         next();
